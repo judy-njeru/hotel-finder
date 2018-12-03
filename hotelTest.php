cf21ec7fb98a45b126cf2c996762b75a8f42b5d8
Birna Birna <birnabirna@Birnas-MacBook-Pro-2.local>
2018-12-03 18:17:33 +0100

    added price to search results, created templates, cleaned up and commented like crazy

--- hotelTest.php
+++ hotelTest.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
-    <!-- <script src="main.js"></script> -->
     <style>
         .starsContainer{
             position:relative;
@@ -42,71 +40,105 @@
     <div id="boxInfo">
         <?php
             ini_set('display_errors', 1);
+            // svg image for stars rating
+            require_once 'components/svg-stars.php';
+
+            // hotel div template
+            $sHotelDiv = "<div>#name#</div>
+                            <div>#address#</div>
+                            <div><span>#rating#</span>
+                            <span class='starsContainer'>
+                                $svgStars
+                                <div class='starsBackground'></div>
+                                <div class='starsFill' style='width:calc(60px*(#rating#/5)'></div>
+                            </span>
+                            </div>
+                            <div>#description#</div>
+                            <div>
+                            #airportPickup#
+                            #wifi#
+                            #airConditioning#
+                            #roomService#
+                            #meetingRooms#
+                            #freeParking#
+                            </div>";
+
+            // get hotel data from the id
             $sId = $_GET['id'];
             $sData = file_get_contents('data.txt');
             $jData = json_decode($sData);
             $jHotel = $jData->hotels->$sId;
+            // redirect if !id 
             if (!$jHotel){
                 header('Location:hotelsTest.php');
             }
-            echo "<div>$jHotel->name</div>
-            <div>$jHotel->address</div>
-            <div><span>$jHotel->rating</span>
-            <span class='starsContainer'>
-                <svg class='starsShape' width='60px' height='12px' viewBox='0 0 60 12' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
-                <g id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'>
-                    <path d='M0,0 L60,0 L60,12 L0,12 L0,0 Z M6,9 L9.52671151,10.854102 L8.85316955,6.92705098 L11.7063391,4.14589803 L7.76335576,3.57294902 L6,0 L4.23664424,3.57294902 L0.293660902,4.14589803 L3.14683045,6.92705098 L2.47328849,10.854102 L6,9 Z M18,9 L21.5267115,10.854102 L20.8531695,6.92705098 L23.7063391,4.14589803 L19.7633558,3.57294902 L18,0 L16.2366442,3.57294902 L12.2936609,4.14589803 L15.1468305,6.92705098 L14.4732885,10.854102 L18,9 Z M30,9 L33.5267115,10.854102 L32.8531695,6.92705098 L35.7063391,4.14589803 L31.7633558,3.57294902 L30,0 L28.2366442,3.57294902 L24.2936609,4.14589803 L27.1468305,6.92705098 L26.4732885,10.854102 L30,9 Z M42,9 L45.5267115,10.854102 L44.8531695,6.92705098 L47.7063391,4.14589803 L43.7633558,3.57294902 L42,0 L40.2366442,3.57294902 L36.2936609,4.14589803 L39.1468305,6.92705098 L38.4732885,10.854102 L42,9 Z M54,9 L57.5267115,10.854102 L56.8531695,6.92705098 L59.7063391,4.14589803 L55.7633558,3.57294902 L54,0 L52.2366442,3.57294902 L48.2936609,4.14589803 L51.1468305,6.92705098 L50.4732885,10.854102 L54,9 Z' id='Combined-Shape' fill='#fff' fill-rule='nonzero'></path>
-                </g>
-                </svg>
-                <div class='starsBackground'></div>
-                <div class='starsFill' style='width:calc(60px*($jHotel->rating/5)'></div>
-            </span>
-            </div>
-            <div>$jHotel->description</div>
-            <div>";
-            echo $jHotel->amenities->airportPickup ? '<span>✔️Airport Pickup </span>' : '';
-            echo $jHotel->amenities->wifi ? '<span>✔️Free WiFi </span>' : '';
-            echo $jHotel->amenities->airConditioning ? '<span>✔️Air Conditioning </span>' : '';
-            echo $jHotel->amenities->roomService ? '<span>✔️Room Service </span>' : '';
-            echo $jHotel->amenities->meetingRooms ? '<span>✔️Meeting Rooms </span>' : '';
-            echo $jHotel->amenities->freeParking ? '<span>✔️Free Parking </span>' : '';
-            echo "</div>";
+            // check if hotel has a value true/false in amenities, only saves span if true
+            $airportPickup = $jHotel->amenities->airportPickup ? '<span>✔️Airport Pickup </span>' : '';
+            $wifi = $jHotel->amenities->wifi ? '<span>✔️Free WiFi </span>' : '';
+            $airConditioning = $jHotel->amenities->airConditioning ? '<span>✔️Air Conditioning </span>' : '';
+            $roomService = $jHotel->amenities->roomService ? '<span>✔️Room Service </span>' : '';
+            $meetingRooms = $jHotel->amenities->meetingRooms ? '<span>✔️Meeting Rooms </span>' : '';
+            $freeParking = $jHotel->amenities->freeParking ? '<span>✔️Free Parking </span>' : '';
+            // create a temp div from template
+            $sTempHotelDiv = $sHotelDiv;
+            // array containing the placeholders
+            $find = array('#name#','#address#','#rating#','#description#','#airportPickup#','#wifi#','#airConditioning#','#roomService#','#meetingRooms#','#freeParking#');
+            // array containing the values - must be same order as placeholder array
+            $replace = array($jHotel->name,$jHotel->address,$jHotel->rating,$jHotel->description,$airportPickup,$wifi,$airConditioning,$roomService,$meetingRooms,$freeParking);
+            // replace placeholders with values
+            echo str_replace($find,$replace,$sTempHotelDiv);
         ?>
         <div>
             <input value="<?php echo $_GET['checkIn']; ?>" placeholder='dd/mm/yyy'>
             <input value="<?php echo $_GET['checkOut']; ?>" placeholder='dd/mm/yyy'><br>
-            <!-- <input id="nrGuests" placeholder="guests" type="number" min="1" max="10000" value="<?php echo $_GET['guests']; ?>"><br> -->
+            <!-- <input id="nrGuests" placeholder="guests" type="number" min="1" max="10000" value="<?php //echo $_GET['guests']; ?>"><br> -->
             <?php
+                $sRoomDiv = "<div class='marginTop' data-id='#id#'>
+                    <div>#name#</div>
+                    <div>$<span class='boxPrice'>#price#</span> pr. night</div>
+                    <div>beds: <span class='boxBeds'>#beds#<span></div>
+                    <div>availability: #availability#</div>
+                    <input class='nrRooms' type='number' placaholder='0' min='0' max='#availability#' value='#rooms#'>
+                </div>";
+                // number of guests from the GET
                 $iGuests = $_GET['guests'];
-                $iGuestsTest = $iGuests;
+                // needed to assign guests to rooms
+                $iGuestsNeedRoom = $iGuests;
+                // object that has room type(key) and how many(value)
                 $jRooms = (object)[];
                 
+                // displays the rooms and options
+                // triggers on load
                 foreach ($jHotel->rooms as $key => $jRoom) {
+                    // number of available rooms of this type
                     $iAvailability = $jRoom->availability;
-                    if($iAvailability > 0){
-                        $nrOfThisRoomTypeNeeded = ceil($iGuestsTest/$jRoom->beds);
-                        if($nrOfThisRoomTypeNeeded <= $iAvailability && $iGuestsTest > 0 && $nrOfThisRoomTypeNeeded > 0){
+                    if($iAvailability > 0){ // check if any rooms are available
+                        // number of rooms of this type needed for set number of guests that need room
+                        $nrOfThisRoomTypeNeeded = ceil($iGuestsNeedRoom/$jRoom->beds);
+                        if($nrOfThisRoomTypeNeeded <= $iAvailability && $iGuestsNeedRoom > 0 && $nrOfThisRoomTypeNeeded > 0){ // check if enough rooms of this type for the number of guests that need room
+                            // sets number of rooms of this type are needed
                             $jRooms->$key = $nrOfThisRoomTypeNeeded;
-                            $iGuestsTest = 0;
-                        }elseif($nrOfThisRoomTypeNeeded > $iAvailability && $iGuestsTest > 0 && $nrOfThisRoomTypeNeeded > 0){
+                            $iGuestsNeedRoom = 0;
+                        }elseif($nrOfThisRoomTypeNeeded > $iAvailability && $iGuestsNeedRoom > 0 && $nrOfThisRoomTypeNeeded > 0){ // check if any rooms of this type
+                            // sets number of rooms of this type are available
                             $jRooms->$key = $iAvailability;
-                            $iGuestsTest = $iGuestsTest - ($iAvailability*$jRoom->beds);
-                        }else{
+                            // reduce number of guests need room
+                            $iGuestsNeedRoom = $iGuestsNeedRoom - ($iAvailability*$jRoom->beds);
+                        }else{ // should only happen if no guests need room
                             $jRooms->$key = 0;
                         }
                     }
-                    else{
+                    else{ // should only happen if no room this type is available
                         $jRooms->$key = 0;
                     }
-                    echo "<div class='marginTop' data-id='$key'>
-                        <div>$jRoom->name</div>
-                        <div>$<span class='boxPrice'>$jRoom->price</span> pr. night</div>
-                        <div>beds: <span class='boxBeds'>$jRoom->beds<span></div>
-                        <div>availability: $jRoom->availability</div>
-                        <input class='nrRooms' type='number' placaholder='0' min='0' max='$jRoom->availability' value='";
-                        echo $jRooms->$key;
-                    echo "'>
-                    </div>";
+                    // create a temp div from template
+                    $sTempRoomDiv = $sRoomDiv;
+                    // array containing the placeholders
+                    $find = array('#id#','#name#','#price#','#beds#','#availability#','#rooms#');
+                    // array containing the values - must be same order as placeholder array
+                    $replace = array($key,$jRoom->name,$jRoom->price,$jRoom->beds,$jRoom->availability,$jRooms->$key);
+                    // replace placeholders with values
+                    echo str_replace($find,$replace,$sTempRoomDiv);
                 }
             ?>
             <div class="marginTop">
@@ -120,7 +152,8 @@
 
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
     <script>
-        function updateInfo(){
+        // displays the total according to selection
+        function setTotalInfo(){
             let iRooms = 0
             let iBeds = 0
             let iPrice = 0
@@ -133,8 +166,11 @@
             $('#boxTotalBeds').text(iBeds)
             $('#boxTotalPrice').text('$'+iPrice)
         }
-        $(document).ready(updateInfo())
-        $(document).on('change','.nrRooms',function(){updateInfo()})
+
+        // triggers setTotalInfo when page loads
+        $(document).ready(setTotalInfo())
+        // triggers setTotalInfo when selection changes
+        $(document).on('change','.nrRooms',function(){setTotalInfo()})
     </script>
 </body>
 </html>
--- hotelsTest.php
+++ hotelsTest.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
-    <!-- <script src="main.js"></script> -->
     <style>
         *{
             color:black;
@@ -49,7 +47,7 @@
             <input name="checkInDate" id="txtCheckInDate" placeholder="dd/mm/yyyy" type="text">
             <label for="txtCheckOutDate">check out</label>
             <input name="checkOutDate" id="txtCheckOutDate" placeholder="dd/mm/yyyy" type="text"><br>
-            <input name="guests" id="nrGuests" placeholder="guests" type="number" min="1" max="10000"><br>
+            <input name="guests" id="nrGuests" placeholder="guests" type="number" min="1" max="10000" value="1"><br>
             <input name="airportPickup" id="chkAirportPickup" type="checkbox"><label for="chkAirportPickup">Airport Pickup</label>
             <input name="wifi" id="chkWifi" type="checkbox"><label for="chkWifi">Free Wi-Fi</label>
             <input name="restaurant" id="chkRestaurant" type="checkbox"><label for="chkRestaurant">Restaurant</label>
@@ -58,40 +56,7 @@
             <input name="meetingRooms" id="chkMeetingRooms" type="checkbox"><label for="chkMeetingRooms">Meeting Rooms</label>
             <input name="freeParking" id="chkFreeParking" type="checkbox"><label for="chkFreeParking">Free Parking</label>
         </div>
-        <div id="boxHotels">
-        <?php
-            // $sData = file_get_contents('data.txt');
-            // $jData = json_decode($sData);
-            // $jHotelx = $jData->hotels;
-
-            // foreach ($jHotelx as $key => $jHotel) {
-            //     $sName = $jHotel->name;
-            //     $sAddress = $jHotel->address;
-            //     $sRating = $jHotel->rating;
-            //     echo '<div data-id="'.$key.'">
-            //             <div>'.$sName.'</div>
-            //             <div><span>'.$sRating.'</span>
-            //             <span class="starsContainer">
-            //                 <svg class="starsShape" width="60px" height="12px" viewBox="0 0 60 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            //                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            //                     <path d="M0,0 L60,0 L60,12 L0,12 L0,0 Z M6,9 L9.52671151,10.854102 L8.85316955,6.92705098 L11.7063391,4.14589803 L7.76335576,3.57294902 L6,0 L4.23664424,3.57294902 L0.293660902,4.14589803 L3.14683045,6.92705098 L2.47328849,10.854102 L6,9 Z M18,9 L21.5267115,10.854102 L20.8531695,6.92705098 L23.7063391,4.14589803 L19.7633558,3.57294902 L18,0 L16.2366442,3.57294902 L12.2936609,4.14589803 L15.1468305,6.92705098 L14.4732885,10.854102 L18,9 Z M30,9 L33.5267115,10.854102 L32.8531695,6.92705098 L35.7063391,4.14589803 L31.7633558,3.57294902 L30,0 L28.2366442,3.57294902 L24.2936609,4.14589803 L27.1468305,6.92705098 L26.4732885,10.854102 L30,9 Z M42,9 L45.5267115,10.854102 L44.8531695,6.92705098 L47.7063391,4.14589803 L43.7633558,3.57294902 L42,0 L40.2366442,3.57294902 L36.2936609,4.14589803 L39.1468305,6.92705098 L38.4732885,10.854102 L42,9 Z M54,9 L57.5267115,10.854102 L56.8531695,6.92705098 L59.7063391,4.14589803 L55.7633558,3.57294902 L54,0 L52.2366442,3.57294902 L48.2936609,4.14589803 L51.1468305,6.92705098 L50.4732885,10.854102 L54,9 Z" id="Combined-Shape" fill="#fff" fill-rule="nonzero"></path>
-            //                 </g>
-            //                 </svg>
-            //                 <div class="starsBackground"></div>
-            //                 <div class="starsFill" style="width:calc(60px*('.$sRating.'/5)"></div>
-            //             </span>
-            //             </div>
-            //             <div>'.$sAddress.'</div>
-            //             </div>';
-            //                 // $bMatch = $sName === $sSearch ? true : false; // a. exact match
-            //                 // // echo $bMatch === true ? $jProduct->name : null; // a. exact match
-            //                 // $bMatch = stripos($sName, $sSearch); // stripos - case insensitive
-            //                 // echo $bMatch > -1 ? $jProduct->name : null;
-            //                 // $iMatch = $sName == $sSearch ? $iMatch + 1 : $iMatch;
-            //                 // echo $bMatch === true ? json_encode($jProduct) : null;           
-            // }
-        ?>
-        </div>
+        <div id="boxHotels"></div>
     </div>
 
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
@@ -99,9 +64,11 @@
         const sDefaultCheckIn = '20/12/2018'
         const sDefaultCheckOut = '03/01/2019'
         const iDefaultGuests = 1
-        function hotelDiv(id,name,rating,address,availability){
+
+        // returns the hotel div with set values - works like a template
+        function hotelDiv(id,name,rating,address,availability,price){
             return `<div class="boxHotel" data-id=${id}>
-                        <div>${name}</div>
+                        <div><span>${name}</span></div>
                         <div><span>${rating}</span>
                         <span class="starsContainer">
                             <svg class="starsShape" width="60px" height="12px" viewBox="0 0 60 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -115,8 +82,10 @@
                         </div>
                         <div>${address}</div>
                         <div>Availability: ${availability} rooms</div>
+                        <div> Price from: <span class='boxPrice'>${price}</span></div>
                         </div>`}
 
+        // get the url for the single hotel page
         $(document).on('click','.boxHotel',function(){
             const sId = $(this).attr('data-id')
             const sCheckIn = $('#txtCheckInDate').val() ? $('#txtCheckInDate').val() : sDefaultCheckIn
@@ -125,6 +94,7 @@
             window.location.href = `hotelTest.php?id=${sId}&checkIn=${sCheckIn}&checkOut=${sCheckOut}&guests=${iGuests}`
         })
 
+        // display hotels when page is ready
         $(document).ready(function(){
             $('#txtCheckInDate').val(sDefaultCheckIn)
             $('#txtCheckOutDate').val(sDefaultCheckOut)
@@ -132,19 +102,16 @@
                 for (const key in data.hotels) {
                     const jHotel = data.hotels[key]
                     let iAvailability = 0
+                    iPrice = getPrice(jHotel)
                     for (const room in jHotel.rooms) {
                         iAvailability += jHotel.rooms[room].availability
                     }
-                    $('#boxHotels').append(hotelDiv(key,jHotel.name,jHotel.rating,jHotel.address,iAvailability))
+                    $('#boxHotels').append(hotelDiv(key,jHotel.name,jHotel.rating,jHotel.address,iAvailability,iPrice))
                 }
             },'json')
         })
 
-        // $(document).on('change','#nrGuests',function(){
-        //     // console.log($(this).val())
-            
-        // })
-    
+        // update results when filters change
         $(document).on('change','#boxFilters',function(e){
             $('#boxHotels').html('')
             const aChecked = $('#boxFilters input:checkbox:checked')
@@ -163,6 +130,7 @@
                     }
                     let iAvailabilityBeds = 0
                     let iAvailabilityRooms = 0
+                    let iPrice = getPrice(jHotel)
                     for (const room in jHotel.rooms) {
                         iAvailabilityBeds += jHotel.rooms[room].availability * jHotel.rooms[room].beds
                         iAvailabilityRooms += jHotel.rooms[room].availability
@@ -171,13 +139,34 @@
                         jChecked[key] = false
                     }
                     if(jChecked[key] == true){
-                        $('#boxHotels').append(hotelDiv(key,jHotel.name,jHotel.rating,jHotel.address,iAvailabilityRooms))
+                        $('#boxHotels').append(hotelDiv(key,jHotel.name,jHotel.rating,jHotel.address,iAvailabilityRooms,iPrice))
                         bNoResults = false
                     }
                 }
                 if(bNoResults)$('#boxHotels').append('<div>no results :-(</div>')
             },'json')
         })
+
+        // get the price for the set number of guests
+        // same logic as in single hotel page
+        function getPrice(data){
+            const iGuests = $('#nrGuests').val()
+            let iGuestsTest = iGuests
+            let iPrice = 0
+            for (const key in data.rooms) {
+                const jRoom = data.rooms[key]
+                let iAvailability = jRoom.availability
+                var iNrOfRoomTypeNeeded = Math.ceil(iGuestsTest/jRoom.beds)
+                if(iNrOfRoomTypeNeeded <= iAvailability && iGuestsTest > 0 && iNrOfRoomTypeNeeded > 0){
+                    iPrice = iPrice + iNrOfRoomTypeNeeded * jRoom.price
+                    iGuestsTest = 0
+                }else if(iNrOfRoomTypeNeeded > iAvailability && iGuestsTest > 0 && iNrOfRoomTypeNeeded > 0){
+                    iPrice = iPrice + iAvailability * jRoom.price
+                    iGuestsTest = iGuestsTest - (iAvailability*jRoom.beds)
+                }
+            }
+            return iPrice
+        }
     </script>
 </body>
 </html>
