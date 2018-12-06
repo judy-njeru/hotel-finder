3394291021bda8cf89cc31209a30afa626a35a2e
andrinja <andra.zeberga@gmail.com>
2018-12-06 13:55:09 +0100

    style hotel search result boxes

--- hotelsTest.php
+++ hotelsTest.php
@@ -103,6 +103,7 @@
                             <i class="material-icons">star_rate</i>
                         </span>
                     </div>
+
                 </div>
             </div>
             <div class="divider"></div>
@@ -158,22 +159,36 @@
         function hotelDiv(id,name,rating,address,availability,price,img){
             return `<div class="boxHotel" data-id=${id}>
                         <div class="boxImg" style="background-image:url(${img})"></div>
-                        <div>${name}</div>
-                        <div><span>${rating}</span>
-                        <span class="starsContainer">
-                            <svg class="starsShape" width="60px" height="12px" viewBox="0 0 60 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <path d="M0,0 L60,0 L60,12 L0,12 L0,0 Z M6,9 L9.52671151,10.854102 L8.85316955,6.92705098 L11.7063391,4.14589803 L7.76335576,3.57294902 L6,0 L4.23664424,3.57294902 L0.293660902,4.14589803 L3.14683045,6.92705098 L2.47328849,10.854102 L6,9 Z M18,9 L21.5267115,10.854102 L20.8531695,6.92705098 L23.7063391,4.14589803 L19.7633558,3.57294902 L18,0 L16.2366442,3.57294902 L12.2936609,4.14589803 L15.1468305,6.92705098 L14.4732885,10.854102 L18,9 Z M30,9 L33.5267115,10.854102 L32.8531695,6.92705098 L35.7063391,4.14589803 L31.7633558,3.57294902 L30,0 L28.2366442,3.57294902 L24.2936609,4.14589803 L27.1468305,6.92705098 L26.4732885,10.854102 L30,9 Z M42,9 L45.5267115,10.854102 L44.8531695,6.92705098 L47.7063391,4.14589803 L43.7633558,3.57294902 L42,0 L40.2366442,3.57294902 L36.2936609,4.14589803 L39.1468305,6.92705098 L38.4732885,10.854102 L42,9 Z M54,9 L57.5267115,10.854102 L56.8531695,6.92705098 L59.7063391,4.14589803 L55.7633558,3.57294902 L54,0 L52.2366442,3.57294902 L48.2936609,4.14589803 L51.1468305,6.92705098 L50.4732885,10.854102 L54,9 Z" id="Combined-Shape" fill="#fff" fill-rule="nonzero"></path>
-                            </g>
-                            </svg>
-                            <div class="starsBackground"></div>
-                            <div class="starsFill" style="width:calc(60px*(${rating}/5)"></div>
-                        </span>
-                        </div>
-                        <div>${address}</div>
-                        <div>Availability: ${availability} rooms</div>
-                        <div> Price from: ${price} DKK per night</div>
-                        </div>`}
+                        <div class="hotel-details">
+                            <div class="hotel-name">${name}</div>
+                            <div class="star-items">
+                                <span>${rating}</span>
+                                <span class="starsContainer">
+                                    <svg class="starsShape" width="60px" height="12px" viewBox="0 0 60 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <path d="M0,0 L60,0 L60,12 L0,12 L0,0 Z M6,9 L9.52671151,10.854102 L8.85316955,6.92705098 L11.7063391,4.14589803 L7.76335576,3.57294902 L6,0 L4.23664424,3.57294902 L0.293660902,4.14589803 L3.14683045,6.92705098 L2.47328849,10.854102 L6,9 Z M18,9 L21.5267115,10.854102 L20.8531695,6.92705098 L23.7063391,4.14589803 L19.7633558,3.57294902 L18,0 L16.2366442,3.57294902 L12.2936609,4.14589803 L15.1468305,6.92705098 L14.4732885,10.854102 L18,9 Z M30,9 L33.5267115,10.854102 L32.8531695,6.92705098 L35.7063391,4.14589803 L31.7633558,3.57294902 L30,0 L28.2366442,3.57294902 L24.2936609,4.14589803 L27.1468305,6.92705098 L26.4732885,10.854102 L30,9 Z M42,9 L45.5267115,10.854102 L44.8531695,6.92705098 L47.7063391,4.14589803 L43.7633558,3.57294902 L42,0 L40.2366442,3.57294902 L36.2936609,4.14589803 L39.1468305,6.92705098 L38.4732885,10.854102 L42,9 Z M54,9 L57.5267115,10.854102 L56.8531695,6.92705098 L59.7063391,4.14589803 L55.7633558,3.57294902 L54,0 L52.2366442,3.57294902 L48.2936609,4.14589803 L51.1468305,6.92705098 L50.4732885,10.854102 L54,9 Z" id="Combined-Shape" fill="#fff" fill-rule="nonzero"></path>
+                                    </g>
+                                    </svg>
+                                    <div class="starsBackground"></div>
+                                    <div class="starsFill" style="width:calc(60px*(${rating}/5)"></div>
+                                </span>
+                            </div>
+                
+                            <div class="description-items">
+                                <i class="material-icons">location_on</i>
+                                <span>${address}</span>
+                            </div>
+                            <div class="description-items">
+                                <i class="material-icons">hotel</i>
+                                <span>${availability} rooms</span>
+                            </div>
+                            <div class="price-items">
+                                <div class="amount">${price} DKK</div>
+                                <div class="per-night"> per night</div>
+                            </div>
+                            
+                        <div>
+                    </div>`}
 
         // get the url for the single hotel page
         $(document).on('click','.boxHotel',function(){
